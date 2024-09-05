51ea349e0c35787fc6e53352d6e5c7d693d6b328
abbishek <aram0057@student.monash.edu>
2024-09-05 12:49:30 +0700

    epic 2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('database/', include('database.urls')),
+    path('', include('database.urls')),  # Redirect the root URL to the 'database' app
 ]
+
--- backend/database/views.py
+++ backend/database/views.py
@@ -1,3 +1,9 @@
+# views.py
 from django.shortcuts import render
+from .models import Centre, Waste, MelbourneSuburbs
+
+def display_data(request):
+    centres = Centre.objects.select_related('waste').all()  # Fetch all centres and related waste type
+    return render(request, 'centres_template.html', {'centres': centres})
 
 # Create your views here.
